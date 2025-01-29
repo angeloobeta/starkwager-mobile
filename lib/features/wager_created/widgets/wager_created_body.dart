2e8a4406ed614539d34d4041feab38fd9dac08e4
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2025-01-29 10:28:15 +0100

    fix: changes

--- lib/features/wager_created/widgets/wager_created_body.dart
+++ lib/features/wager_created/widgets/wager_created_body.dart
@@ -165,7 +165,9 @@ class _AccountCreatedBodyState extends ConsumerState<WargerCreatedBody> {
                   title: 'backHome'.tr(),
                   buttonColor: AppColors.white,
                   textColor: AppColors.blue950,
-                  onPressed: () => context.go(Routes.create_wager_summary),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                   textInputState: textInputState),
             ],
           ),
